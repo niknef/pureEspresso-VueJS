b27c0b7c67513aa63240778e38608d78b6604868
NikNef <nicolas.firpo@davinci.edu.ar>
2024-07-02 19:40:00 -0300

    add last update to Vue settings

--- scripts/app.js
+++ scripts/app.js
@@ -75,7 +75,8 @@ Vue.component('modal-component', {
                 subscription: '',
                 preferenciasCafe: '',
                 notas: ''
-            }
+            },
+            arr: []
         };
     },
     //traemos la props de modalvisible para utilizar el v-show con la modal
@@ -148,7 +149,7 @@ Vue.component('modal-component', {
                             </div>
                             
                             <div class="text-center">
-                            <button type="submit" class="btn btn-dark mx-auto">Suscribirse</button>
+                            <button type="submit" class="btn btn-dark mx-auto" @click="guardar(form)">Suscribirse</button>
                             </div>
                         </form>
                     </div>
@@ -158,34 +159,92 @@ Vue.component('modal-component', {
     `,
     methods: {
         closeModal() {
-            // Emite un evento para cerrar el modal en la instancia principal de Vue
             this.$emit('close-modal');
         },
         submitForm() {
-            // Emite un evento para manejar el envío del formulario
-            this.$emit('form-submitted', this.form);
-            this.closeModal(); // Cierra el modal después de enviar el formulario
+            // Hacemos una copia profunda de los datos del formulario antes de emitirlos
+            const formDataCopy = JSON.parse(JSON.stringify(this.form));
+            this.$emit('form-submitted', formDataCopy);
+            this.guardar(formDataCopy);
+            modalvisible = false // Guardamos los datos en localStorage
+            this.resetForm(); // Reseteamos el formulario
+        },
+        guardar(form) {
+            console.log('Guardando en Local Storage:', form);
+            if (!localStorage.local) {
+                this.arr = [];
+            } else {
+                this.arr = JSON.parse(localStorage.getItem("local"));
+            }
+            this.arr.push(form);
+            localStorage.setItem("local", JSON.stringify(this.arr));
+        },
+        resetForm() {
+            // Reseteamos el formulario después de guardar y emitir los datos
+            this.form = { nombre: '', apellido: '', email: '', subscription: '', preferenciasCafe: '', notas: '' };
+        }
+    }
+});
+
+Vue.component('modal-gracias', {
+    props: ['completed', 'alerta', 'usuarios'],
+    template: `
+        <div :class="[completed ? 'visible' : 'visually-hidden']" class="modal-backdrop">
+            <div class="modal-dialog">
+                <div class="modal-content bg-body-tertiary rounded p-4">
+                    <div class="modal-header d-flex justify-content-between mb-3">
+                        
+                        <h2 class="modal-title fs-5 fw-bold me-4" id="exampleModalLabel">Bienvenido/a {{usuarios[0].nombre | capitalize}}</h2>
+                        
+                        <button type="button" class="btn-close" @click="closeModal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body text-center content-modal-gracias">
+                        <div class="bienvenido mx-auto my-2"></div>
+                        <h3 class="fw-bold">{{alerta}}</h3>
+                        <h4 class="fw-bold">Pack = {{usuarios[0].subscription | capitalize}}</h4>
+                        <p class="lead fw-bold">Recibirás un correo de confirmación con los detalles de tu suscripción al siguiente E-mail: {{usuarios[0].email | capitalize}}</p>
+                        <p class="fw-bold">¡Gracias por formar parte de nuestra comunidad de amantes por el café, en nuestra página encontraras una recomendación especial para tí!</p>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    `,
+    methods: {
+        closeModal() {
+            this.$emit('close-modal');
         }
     }
 });
 
 // Creamos el componente para los packs
 Vue.component('packs-component', {
-    //traemos con props packs el array de packs
     props: ['packs'],
     template: `
-        <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3" id="opciones">
-            <div v-for="pack in packs" :key="pack.name" class="bg-light-coffee me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center rounded overflow-hidden">
-                <div class="my-3 p-3">
-                    <h2 class="display-5">{{ pack.name }}</h2>
-                    <p class="lead fw-bold">{{ pack.price }}</p>
-                    <p class="lead">{{ pack.description }}</p>
-                </div>
-                <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
-                    <img :src="pack.imageUrl" :alt="pack.imageAlt" class="img-fluid drop-shadow" width="300px">
+    <div class="container my-5">
+        <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+            <h2 class="display-4 fw-bold text-brown-light">Nuestros Packs</h2>
+            <p class="fs-5 text-light">Descubre una experiencia única con nuestra selección de packs de café. Cada mes, te traemos una cuidada combinación de los mejores granos, accesorios esenciales y consejos para que disfrutes de tu café como nunca antes.</p>
+        </div>
+        
+        <div class="row justify-content-center text-center">
+            <div class="col-md-4" v-for="(pack, index) in packs" :key="index">
+                <div class="card mb-4 rounded-3 shadow-sm">
+                    <div class="card-header py-3">
+                        <h3 class="my-0 fw-bold">{{ pack.name }}</h3>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title pricing-card-title fw-bold">{{ pack.price }}<small class="text-body-secondary fw-light">/mo</small></h4>
+                        
+                        <img :src="pack.imageUrl" :alt="pack.imageAlt" class="img-fluid drop-shadow" width="300px">
+                        
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li v-for="item in pack.items" :key="item" class="fw-bold">{{ item }}</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
     `
 });
 
@@ -234,10 +293,11 @@ Vue.component('products-component', {
     //llamamos con props al array products
     props: ['products'],
     template: `
-        <div class="album py-5 bg-dark">
+        <div class="album py-5 bg-dark text-center">
             <div class="container">
+            <h2  class="fw-bold text-brown-light mb-4">Proximos Productos</h2>
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                <div class="col" v-for="product in products" :key="product.name">
+                <div class="col" v-for="(product, index) in products" :key="index">
                 <div class="card shadow-sm">
                     <img :src="product.imageUrl" :alt="product.imageAlt" class="rounded-top">
                     <div class="card-body bg-light-coffee">
@@ -253,6 +313,32 @@ Vue.component('products-component', {
     `
 });
 
+Vue.component('coffees-component', {
+    props: ['coffees'],
+    template: `
+    <div v-if="coffees.length > 0" class="container">
+        <div class="text-center">
+            <h2 class="display-4 fw-bold text-brown-light my-3">Nuestras Recomendaciones</h2>
+        </div>
+        <div class="row row-cols-1 row-cols-md-2 g-4" id="opciones">
+            <div v-for="(coffee, index) in coffees" :key="index" class="col">
+                <div class="card h-100 bg-gris-claro text-center rounded overflow-hidden">
+                    <div class="card-body">
+                        <h2 class="card-title display-5">{{ coffee.name }}</h2>
+                        <p class="lead fw-bold">{{ coffee.nacionalidad }}</p>
+                        <p class="lead">{{ coffee.descripcion }}</p>
+                    </div>
+                    <div class="bg-body shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
+                    <img :src="coffee.img" :alt="coffee.name" class="img-fluid drop-shadow" width="300px">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+});
+
+
 //creamos componente para el footer
 Vue.component('footer-component', {
     template: `
@@ -266,26 +352,31 @@ Vue.component('footer-component', {
             </ul>
         </footer>
     `
-  });
-  
+});
+
 
 // Instancia principal de Vue
 var app = new Vue({
     el: '.app',
     data: {
         modalvisible: false, // Asegúrate de usar 'modalvisible' en lugar de 'modalVisible'
+        completed: false,
+        preferenciaseleccionada: false,
+        userCoffeePreference: '',
+        usuarios: [], 
+        alerta: "¡Gracias por suscribirte!",// Array para almacenar los usuarios
         packs: [
             {
                 name: 'Starter Pack',
-                price: '$3usd /Mes',
-                description: 'Selección de Cafés (3 variantes), Guía de cata básica, Tutoriales para hacer un buen café.',
+                price: '$3usd',
+                items: ['3 variantes de café', 'Guía de cata básica', 'Tutoriales online', 'Newsletter Comunidad', 'Receta con café'],
                 imageUrl: './img/starterpack.png',
                 imageAlt: 'Ilustración del pack starter'
             },
             {
                 name: 'Premium Pack',
-                price: '$5usd /Mes',
-                description: 'Selección de Cafés (5 variantes), Guía de cata avanzada, Regalo adicional, Descuentos exclusivos.',
+                price: '$5usd',
+                items: ['5 variantes de café', 'Guía de cata avanzada', 'Tutoriales online + Newsletter', 'Descuentos exclusivos', 'Regalo adicional'],
                 imageUrl: './img/premiumpack.png',
                 imageAlt: 'Ilustración del pack premium'
             }
@@ -309,22 +400,74 @@ var app = new Vue({
                 imageUrl: './img/molinillo-profesional.jpg',
                 imageAlt: 'Molinillo profesional para granos de café'
             }
+        ],
+        coffees: [
+            {
+                name: 'Café Colombiano',
+                nacionalidad: 'Colombia',
+                img: './img/cafe-colombiano.png',
+                intensidad: 'Medio',
+                descripcion: 'Un café suave con una acidez balanceada y notas de frutas tropicales. Es conocido por su consistencia y sabor excepcional.'
+            },
+            {
+                name: 'Café Etíope Yirgacheffe',
+                nacionalidad: 'Etiopía',
+                img: './img/cafe-ethiopia.png',
+                intensidad: 'Suave',
+                descripcion: 'Este café ofrece un perfil de sabor floral con toques de cítricos y té. Es apreciado por su complejidad y elegancia.'
+            },
+            {
+                name: 'Café Brasileño Santos',
+                nacionalidad: 'Brasil',
+                img: './img/cafe-brasilero.png',
+                intensidad: 'Medio',
+                descripcion: 'Un café con cuerpo y notas de chocolate y nueces. Es uno de los cafés más populares debido a su suavidad y dulzura natural.'
+            },
+            {
+                name: 'Café Keniano AA',
+                nacionalidad: 'Kenia',
+                img: './img/cafe-kenia.png',
+                intensidad: 'Intenso',
+                descripcion: 'Con una acidez vibrante y sabores de frutas rojas y vino, este café es potente y complejo, ideal para quienes buscan una experiencia intensa.'
+            },
+            {
+                name: 'Café de Sumatra Mandheling',
+                nacionalidad: 'Indonesia',
+                img: './img/cafe-sumatra.png',
+                intensidad: 'Intenso',
+                descripcion: 'Con cuerpo pesado y bajo nivel de acidez, este café tiene sabores terrosos y de especias, perfecto para quienes prefieren un perfil más robusto.'
+            },
+            {
+                name: 'Café de Costa Rica Tarrazú',
+                nacionalidad: 'Costa Rica',
+                img: './img/cafe-costarica.png',
+                intensidad: 'Suave',
+                descripcion: 'Un café limpio y brillante con acidez viva y notas de cítricos y cacao. Es muy equilibrado y agradable para cualquier momento del día.'
+            }
         ]
     },
     methods: {
         toggleModal() {
-            // Cambia el estado de modalvisible a true cuando se de clic en 'Suscríbete' o 'Suscríbete ahora!'
             this.modalvisible = !this.modalvisible;
         },
-        
         handleFormSubmission(formData) {
             console.log('Form submitted:', formData);
-            
+            this.usuarios.unshift(formData);
+            console.log('Usuarios:', this.usuarios);
+            this.completed = true; // Indicar que el formulario se ha completado
+            this.userCoffeePreference = formData.preferenciasCafe; // Almacenar la preferencia de café del usuario
+            this.preferenciaseleccionada = true;
         },
-        
         closeModal() {
-            // Establece modalvisible en false cuando se cierra el modal
             this.modalvisible = false;
+            this.completed = false;
+        }
+    },
+    computed: {
+        filteredCoffees() {
+            // Filtra los cafés según la preferencia del usuario
+            if (!this.userCoffeePreference) return [];
+            return this.coffees.filter(coffee => coffee.intensidad.toLowerCase() === this.userCoffeePreference.toLowerCase());
         }
     }
 });
